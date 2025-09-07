f1c93266482bea9bb4e1b359746662750ae29a16
shahsneha748-hash <shah.sneha748@gamil.com>
2025-09-07 13:29:44 +0545

    java file updated

---
+++ Sneha/Escape.java
@@ -0,0 +1,11 @@
+public class Escape{
+    public static void main(String[]args) {
+        System.out.print("A \"quoted\" String is\n");
+        System.out.print("\'much\' better if you learn\n");
+        System.out.print("the rules of \"escape sequences.\"\n");
+        System.out.print("Also, \"\" represents an empty\n");
+        System.out.print("String. Don't forget: use \\\"\n");
+        System.out.print("instead of \"!\n");
+        System.out.print("\"is not the same as\"\n");
+    }
+}
