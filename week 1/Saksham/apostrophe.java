67e78112fcfd0699fe37a47809ea1f0eedd28a1c
Saksham Raj Shrestha <sakshamshrestha760@gmail.com>
2025-09-07 12:20:36 +0545

    Add files via upload
    ...

---
+++ week 1/Saksham/apostrophe.java
@@ -0,0 +1,12 @@
+/* Write a program that prints the following output:
+A "quoted" String is 'much' better if you learn the rules of "escape sequences." Also, "" represents an empty String.  Don't forget: use \"
+instead of " !'' is not the same as "
+
+
+ */
+
+public class apostrophe {
+    public static void main(String[] args) {
+        System.out.println("A\"quoted\" string is\n'much' better if you learn\n the rules of \"escape\" sequences.\nAlso,\"\"represents an empty String.");
+    }
+}
