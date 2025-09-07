a734390b6984b5bec2b22551aac9e572a849ed14
Jibesh16 <jibeshshresthaths@gmail.com>
2025-09-07 22:28:09 +0545

    adding folder

---
+++ week 1/Jibesh/Voting.java
@@ -0,0 +1,7 @@
+public class Voting {
+    public static void main(Strings[] args) {
+        int age= 67;
+        String result = (age > 18) ?"You are ready to vote": "You are not";
+        System.out.println(reult);
+    }
+}
