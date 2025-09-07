fb2546d2097ab23d695e3e3873aa3ce380c1005d
coffeespasity <coffeespasity@gmail.com>
2025-09-07 14:24:35 +0545

    added task for week 1

---
+++ week 1/samridha/f.java
@@ -0,0 +1,49 @@
+public class f {
+    public static void main(String[] args) {
+        //1 hello world
+        System.out.println("Hello World!");
+
+        //Modified Hello Program
+        System.out.print("Hello World!\n");
+        System.out.print("This is my first program\n");
+        System.out.print("I am on module ST4003CEM\n");
+
+        //Escape Sequences
+        System.out.println("A \"quoted\" String is");
+        System.out.println("'much' better if you learn");
+        System.out.println("the rules of \"escape sequences.\"");
+        System.out.println("Also, \"\" represents an empty");
+        System.out.println("String. Don't forget: use \\\"");
+        System.out.println("instead of \" !");
+        System.out.println("'' is not the same as \"");
+
+        //Star Pattern(triangle)
+         System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****"); System.out.println("*");
+       
+        //Star Pattern (Rectangle)
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+
+        //Well-Formed Java Program
+        System.out.println("A well-formed Java program has a main method with { and } braces.");
+        System.out.println();
+        System.out.println("A System.out.println statement has ( and ) and usually a String that starts and ends with a \" character.");
+        System.out.println("(But we type \\\" instead!)");
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"?  Or between a \" and a \\\"?");
+        System.out.println();
+        System.out.println("One is what we see when we're typing our program. The other is what appears on the \"console.\"");
+
+        
+        
+
+    }
+    
+}
