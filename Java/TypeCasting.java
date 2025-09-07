4afce4483df394d8adb75fb3dd2c9253d2e6ce42
Jibesh16 <jibeshshresthaths@gmail.com>
2025-09-07 21:04:10 +0545

    java

---
+++ Java/TypeCasting.java
@@ -0,0 +1,9 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        int num=8;
+        double num1=num;
+        num1=8.2;
+        int num2=(int) num1;
+        System.out.println("num2="+num2);
+    }
+}
